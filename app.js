b2625bba2c5e9ed18abecdfe92110c26e70738f3
mgholden <mgholden27@gmail.com>
2022-10-23 15:41:29 -0400

    Thinking through the process of the game and what needs to be done.  Pseudocode ideas.

---
+++ app.js
@@ -0,0 +1,12 @@
+const cells = document.querySelectorAll('.cell');
+const gameStatus = document.querySelector('#game-status');
+const resetButton = document.querySelector('#reset-button');
+
+//Set the placeholders for the initial state of the game.
+//9 placeholders are needed for the 9 cells in the game.
+//Keep track of the current player and when the game is running.
+//Set the winning combinations by using an array of indices
+//Only focus on the three across, three down, and two diagonals
+
+
+
