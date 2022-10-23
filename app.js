c09dd03e3ddca3176f289910d150f686a5185c16
mgholden <mgholden27@gmail.com>
2022-10-23 17:23:34 -0400

    Initialize game working and changePlayer working

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const boxes = document.querySelectorAll('.box');
-const gameStatus = document.querySelector('#game-status');
-const resetButton = document.querySelector('#reset-button');
+const gameStatus = document.querySelector('#gameStatus');
+const resetButton = document.querySelector('#resetButton');
 
 //Set the winning combinations by using an array of indices
 //Only focus on the three across, three down, and two diagonals
@@ -16,22 +16,23 @@ const winCombinations = [
 ];
 
 //Set the placeholders for the initial state of the game.
-//9 placeholders are needed for the 9 cells in the game.
+//9 placeholders are needed for the 9 boxes in the game.
 let options = ['', '', '', '', '', '', '', '', ''];
 //Keep track of the current player and when the game is running.
 let currentPlayer = 'X';
 let running = false;
 
 //Need to initialize the game.
+initializeGame();
 //This function will be called when the page loads.
 function initializeGame(){
-    //When a box is clicked, call the boxClicked function.
+    //When a cell is clicked, call the cellClicked function.
     boxes.forEach(box => box.addEventListener('click', boxClicked));
     //on click of the reset button, reset the game.
     resetButton.addEventListener('click', resetGame);
     //Who's turn is it?
     gameStatus.textContent = `${currentPlayer}'s turn`;
-
+    running = true;
 }
 //Clicking the boxes and updating the boxes.
 function boxClicked(){
@@ -42,14 +43,27 @@ function boxClicked(){
         return;
     }
     updateBox(this, boxIndex);
+    //Test changePlayer();
     checkForWin();
 }
 
 function updateBox(box, index){
-
+    options[index] = currentPlayer;
+    box.textContent = currentPlayer;
 }
 //Which player is playing/change between players.
+function changePlayer(){
+    //if the current player is X, change it to O.
+    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    gameStatus.textContent = `${currentPlayer}'s turn`;
+}
 //Check for a winner.
+function checkForWin(){
+
+}
+
+//Reset the game.
+function resetGame(){}
 
 
 
